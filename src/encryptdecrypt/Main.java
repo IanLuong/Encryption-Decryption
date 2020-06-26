3ab76cbc8a84be6fe26f4f3dcc7cec4c7b57ff1e
IanLuong <43543794+IanLuong@users.noreply.github.com>
2020-06-27 00:23:11 +0100

    Add encryption for user input and key

--- src/encryptdecrypt/Main.java
+++ src/encryptdecrypt/Main.java
@@ -1,44 +1,44 @@
 package encryptdecrypt;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        char[] message = "we found a treasure!".toCharArray();
+        char[] message = scanner.nextLine().toCharArray();
+        int key = scanner.nextInt();
 
-        char[] letters = {'A', 'B', 'C', 'D', 'E',
+        for (int i = 0; i < message.length; i++) {
+            message[i] = encryptCharacter(message[i], key);
+        }
+
+
+        System.out.println(String.valueOf(message));
+    }
+    
+    private static char encryptCharacter(char character, int key) {
+        final char[] letters = {'A', 'B', 'C', 'D', 'E',
                 'F', 'G', 'H', 'I', 'J',
                 'K', 'L', 'M', 'N', 'O',
                 'P', 'Q', 'R', 'S', 'T', 'U',
                 'V', 'W', 'X', 'Y', 'Z'};
+        int charIndex = 0;
+        boolean isLetter = Character.toString(character).matches("[a-zA-Z]");
+        boolean isLowerCase = Character.toString(character).matches("[a-z]");
 
-        char[] reverseLetters = {'Z', 'Y', 'X', 'W', 'V',
-                'U', 'T', 'S', 'R', 'Q',
-                'P', 'O', 'N', 'M', 'L',
-                'K', 'J', 'I', 'H', 'G', 'F',
-                'E', 'D', 'C', 'B', 'A'};
-
-
-        for (int j = 0; j < message.length; j++) {
-            if (Character.toString(message[j]).matches("[a-z]")) {
-                for (int i = 0; i < 26; i++) {
-                    if (Character.toUpperCase(message[j]) == letters[i]) {
-                        message[j] = Character.toLowerCase(reverseLetters[i]);
-                        break;
-                    }
-                }
-            } else if (Character.toString(message[j]).matches("[A-Z]")) {
-                for (int i = 0; i < 26; i++) {
-                    if (message[j] == letters[i]) {
-                        message[j] = reverseLetters[i];
-                        break;
-                    }
+        if (isLetter) {
+            for (int i = 0; i < 26; i++) {
+                if (letters[i] == Character.toUpperCase(character)) {
+                    charIndex = i;
+                    break;
                 }
             }
+            return isLowerCase ? Character.toLowerCase(letters[(charIndex + key) % 26]) : letters[(charIndex + key) % 26];
+        }
+        else {
+            return character;
         }
 
-        System.out.println(String.valueOf(message));
     }
+
 }
